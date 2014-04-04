2ecb8ea287c3ef39d96efb73e703a12ef74b8e07
Dejniel <danik12@wp.pl>
2014-04-04 15:56:20 +0200

    cos zepsulem

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-
-/**
-	* Node.js Login Boilerplate
-	* More Info : http://bit.ly/LsODY8
-	* Copyright (c) 2013 Stephen Braitsch
-**/
-
 var express = require('express');
 var http = require('http');
 var app = express();
 
 app.configure(function(){
-	app.set('port', 8080);
+	app.set('port', 12345);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
-	app.locals.pretty = true;
+//	app.locals.pretty = false;
 //	app.use(express.favicon());
 //	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'super-duper-secret-secret' }));
+	app.use(express.session({ secret: 'qwertyuioplkjhgfdsazxcvbnm' }));
 	app.use(express.methodOverride());
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
